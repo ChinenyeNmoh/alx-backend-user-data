6ab0efeff2b87fa4efb4c78ef546d1ab803b4060
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2024-01-17 21:21:12 +0100

    session

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -44,6 +44,7 @@ def login() -> str:
         else:
             return jsonify({"error": "wrong password"}), 401
 
+
 @app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
     """Delete route"""
